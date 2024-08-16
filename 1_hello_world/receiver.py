68ccb1952880c25b6457015bf11da79b2e74c71a
Hiroyuki Shigeta <shigeta@glgl.org>
2024-08-16 20:21:59 +0900

    renamed filename

--- 1_hello_world/receiver.py
+++ 1_hello_world/receiver.py
@@ -1,5 +1,5 @@
 
-import pika
+import pika, sys, os
 
 def main():
     credentials = pika.PlainCredentials("root", "password")
@@ -8,13 +8,20 @@ def main():
     channel = connection.channel()
     channel.queue_declare(queue='hello')
 
-    channel.basic_publish(exchange='',
-                          routing_key='hello',
-                          body='Hello World!')
-    print(" [x] Sent 'Hello World!'")
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
 
-    connection.close()
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- 1_hello_world/sender.py
+++ 1_hello_world/sender.py
@@ -1,5 +1,5 @@
 
-import pika, sys, os
+import pika
 
 def main():
     credentials = pika.PlainCredentials("root", "password")
@@ -8,20 +8,13 @@ def main():
     channel = connection.channel()
     channel.queue_declare(queue='hello')
 
-    def callback(ch, method, properties, body):
-        print(f" [x] Received {body}")
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    connection.close()
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+    main()
