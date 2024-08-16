e4057b7e9394ea4afa9639fb913925d2c853eb4c
Hiroyuki Shigeta <shigeta@glgl.org>
2024-08-16 19:25:42 +0900

    changed file name

--- 1_hello_world/receiver.py
+++ 1_hello_world/receiver.py
@@ -8,11 +8,10 @@ def main():
     channel = connection.channel()
     channel.queue_declare(queue='hello')
 
-    for i in range(10):
-        channel.basic_publish(exchange='',
-                              routing_key='hello',
-                              body='Hello World!')
-        print(" [x] Sent 'Hello World!'")
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
 
     connection.close()
 
